8d714513ef5cfd2b8fed987ac2017bd2ef409c54
edubrunaldi <eduardo.brunaldi.santos@gmail.com>
2015-09-04 00:14:04 -0300

    paralelo faltando a funcao

--- JRParalelo.c
+++ JRParalelo.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <pthread.h>
 
 /*
 MA       = 
@@ -8,6 +9,7 @@ MB       =
 X        =
 OLD_X    =
 ROW_TEST = 
+interval_thread = intervalo de linhas da matriz que cada thread ira ler
 */
 typedef struct matrizes
 {
@@ -16,6 +18,8 @@ typedef struct matrizes
     double *X;
     double *OLD_X; 
     double *ROW_TEST;
+    int J_ORDER;
+    int **interval_thread;
 }MATRIZES;
 
 //double erro (double *X, double *OLD_X, int J_ORDER)
@@ -25,7 +29,7 @@ double erro (MATRIZES *matrizes, int init_interaction)
     double diferenca;
     double divisor = 0;
     double X_aux;
-    for (int i = 0 ; i < J_ORDER; ++i)
+    for (int i = 0 ; i < matrizes->J_ORDER; ++i)
     {
         /* Calcula modulo infinito de Xk - Xk-1 */
         diferenca = (init_interaction)? matrizes->X[i] - matrizes->OLD_X[i] : matrizes->X[i];
@@ -44,51 +48,81 @@ double erro (MATRIZES *matrizes, int init_interaction)
 int main ()
 {
     clock_t end, start;
-    int J_ORDER, J_ROW_TEST, J_ITE_MAX;
+    int J_ROW_TEST, J_ITE_MAX;
     double J_ERROR;
+    int n_threads=4; //4 pois eh o que meu pc possui
     //double **MA, *MB, *X, *OLD_X, *ROW_TEST; 
     MATRIZES *matrizes=NULL;
     /* Inicio do programa - leitura dos valores iniciais */
     start = clock();
     matrizes = (MATRIZES *) malloc(sizeof(MATRIZES));
-    scanf ("%d %d %lf %d", &J_ORDER, &J_ROW_TEST, &J_ERROR, &J_ITE_MAX);
-    matrizes->MA = (double **) malloc (sizeof(double *)*J_ORDER);
-    matrizes->MB = (double *) malloc (sizeof(double )*J_ORDER);
-    matrizes->X = (double *) malloc (sizeof(double )*J_ORDER);
-    matrizes->OLD_X = (double *) malloc (sizeof(double )*J_ORDER);
-    matrizes->ROW_TEST = (double *) malloc (sizeof(double )* (J_ORDER+1));
+    scanf ("%d %d %lf %d", &(matrizes->J_ORDER), &J_ROW_TEST, &J_ERROR, &J_ITE_MAX);
+    matrizes->MA = (double **) malloc (sizeof(double *)*matrizes->J_ORDER);
+    matrizes->MB = (double *) malloc (sizeof(double )*matrizes->J_ORDER);
+    matrizes->X = (double *) malloc (sizeof(double )*matrizes->J_ORDER);
+    matrizes->OLD_X = (double *) malloc (sizeof(double )*matrizes->J_ORDER);
+    matrizes->ROW_TEST = (double *) malloc (sizeof(double )* (matrizes->J_ORDER+1));
+    matrizes->interval_thread = (int **) malloc(sizeof(int *)* n_threads);
+
+    for(int i = 0; i < n_threads; ++i)
+    {
+        // 2 pois conta a linha inicial e final que cada thread vai ler
+        matrizes->interval_thread[i] = malloc(sizeof(int)*2); 
+        if(i==0)
+        {
+            matrizes->interval_thread[i][0] = 0;
+            matrizes->interval_thread[i][1] = matrizes->J_ORDER/n_threads -1;
+        }
+        else
+        {
+            matrizes->interval_thread[i][0] =  
+                                matrizes->J_ORDER/n_threads + matrizes->interval_thread[i-1][0];
+
+            if(i+1 == n_threads)
+            {//garante que quando J_ORDER/n_threads nao der inteiro pegue todas as linhas
+             //deixando a ultima thread com algumas linhas a mais, caso a divisao nao der inteiro
+            
+                matrizes->interval_thread[i][1] = matrizes->J_ORDER-1;
+            }
+            else
+            {
+                matrizes->interval_thread[i][1] = 
+                                matrizes->J_ORDER/n_threads + matrizes->interval_thread[i-1][1];
+            }
+        }
+    }
     
     /* Alocação da matrizes A*/
-    for(int i = 0; i<J_ORDER; ++i)
+    for(int i = 0; i<matrizes->J_ORDER; ++i)
     {
-        matrizes->MA[i] = (double *) malloc (sizeof(double)*J_ORDER);
+        matrizes->MA[i] = (double *) malloc (sizeof(double)*matrizes->J_ORDER);
     }
     
     /* Leitura dos valores de A*/
-    for(int i = 0; i< J_ORDER; ++i)
+    for(int i = 0; i< matrizes->J_ORDER; ++i)
     {
-        for (int j = 0; j<J_ORDER ; ++j)
+        for (int j = 0; j<matrizes->J_ORDER ; ++j)
         {
             scanf ("%lf", &(matrizes->MA[i][j]) );
         }
     }
     
     /* Leitura dos valores de B*/
-    for(int i = 0; i<J_ORDER; ++i)
+    for(int i = 0; i<matrizes->J_ORDER; ++i)
     {
         scanf ("%lf", &(matrizes->MB[i]) );
     }
     printf("tempo de leitura: %lf\n",double(clock() - start)/CLOCKS_PER_SEC);
     start = clock();
 
-    matrizes->ROW_TEST[J_ORDER]=matrizes->MB[J_ROW_TEST];
-    for(int i = 0; i<J_ORDER; ++i)
+    matrizes->ROW_TEST[matrizes->J_ORDER]=matrizes->MB[J_ROW_TEST];
+    for(int i = 0; i<matrizes->J_ORDER; ++i)
         matrizes->ROW_TEST[i] = matrizes->MA[J_ROW_TEST][i]; /*salva a linha de teste*/
     
     /* Encontra as matrizes L*, I, R* de dentro da matrix MA */
-    for(int i = 0; i< J_ORDER; ++i)
+    for(int i = 0; i< matrizes->J_ORDER; ++i)
     {
-        for (int j = 0; j<J_ORDER ; ++j)
+        for (int j = 0; j<matrizes->J_ORDER ; ++j)
         {
             if(i!=j) matrizes->MA[i][j]/= matrizes->MA[i][i];
         }
@@ -104,18 +138,20 @@ int main ()
     //double ERRO = erro(X,NULL, J_ORDER);
     double ERRO = erro(matrizes,0);
     int k;
+
     /* Iterações até atingir o critério de parada */
     for ( k = 0; ERRO > J_ERROR && k < J_ITE_MAX ; ++k)
     {
-        for(int i = 0; i < J_ORDER; ++i)
+
+        /*for(int i = 0; i < matrizes->J_ORDER; ++i)
         {
             matrizes->OLD_X[i] = matrizes->X[i];
             matrizes->X[i] = matrizes->MB[i];
-            for(int j=0; j<J_ORDER; ++j)
+            for(int j=0; j<matrizes->J_ORDER; ++j)
             {
                 matrizes->X[i]-=(matrizes->X[j]* matrizes->MA[i][j]);
             }
-        }
+        }*/
         //ERRO = erro(X,OLD_X, J_ORDER);
         ERRO = erro(matrizes, 1);
         printf("%d\t\t%lf\n", k, ERRO);
@@ -123,22 +159,26 @@ int main ()
     
     printf("Numero de Interacoes: %d\n",k);
     double rowtest = 0;
-    for (int i = 0; i<J_ORDER; ++i)
+    for (int i = 0; i<matrizes->J_ORDER; ++i)
     {
         rowtest+= matrizes->ROW_TEST[i]*matrizes->X[i];
     }
-    printf("RowTest: %d => [%lf] =? %lf\n", J_ROW_TEST, rowtest ,matrizes->ROW_TEST[J_ORDER]);
+    printf("RowTest: %d => [%lf] =? %lf\n", J_ROW_TEST, rowtest ,matrizes->ROW_TEST[matrizes->J_ORDER]);
 
     printf("tempo de execucao: %lf\n",double(clock() - start)/CLOCKS_PER_SEC);
     
-    for(int i = 0; i < J_ORDER ; ++i)
+    for(int i = 0; i < matrizes->J_ORDER ; ++i)
     {
         printf("X[%d] = %lf\n", i, matrizes->X[i]);
     }
     /* Encerramento do programa - Liberação de memória */
-    for(int i = 0; i<J_ORDER; ++i)
+    for(int i = 0; i<matrizes->J_ORDER; ++i)
     {
         free(matrizes->MA[i]);
+    }
+    for(int i =0; i< n_threads; ++i)
+    {
+        free(matrizes->interval_thread[i]);
     }
     
     free(matrizes->MA);
@@ -146,6 +186,7 @@ int main ()
     free(matrizes->X);
     free(matrizes->OLD_X);
     free(matrizes->ROW_TEST);
+    free(matrizes->interval_thread);
     free(matrizes);
     return 0;
 }
